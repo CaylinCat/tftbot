86ae298003c33fe4e0b33c66e91bb7dc69985d1a
CaylinCat <60459726+CaylinCat@users.noreply.github.com>
2025-05-07 17:32:15 -0700

    bash script for playwright setup on render

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y \
+  libgtk-4-1 \
+  libgraphene-1.0-0 \
+  libgstgl-1.0-0 \
+  libgstcodecparsers-1.0-0 \
+  libavif15 \
+  libenchant2-2 \
+  libsecret-1-0 \
+  libmanette0 \
+  libgles2
